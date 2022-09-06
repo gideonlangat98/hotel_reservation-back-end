4dd1b434762451fb7d9f4660eb176f824b8a5a8e
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-06 13:15:06 +0300

    add routes of hotels and customer reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,60 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  #Hotel routes
+  get "/hotels" do
+    hotels = hotel.all
+    hotels.to_json
+  end
+
+  post "/hotels" do
+    hotel = hotel.create(
+      name: params[:name],
+      location: params[:location],
+      price: params[:price]
+    )
+    hotel.to_json
+  end
+
+  patch "/hotels/:id" do
+    hotels = hotels.find(params[:id])
+    hotels.update(
+      name: params[:name],
+      price: params[:price]
+    )
+    hotels.to_json
+  end
+
+  delete "/hotels/:id" do
+    hotels = hotels.find(params[:id])
+    hotels.destroy
+    hotels.to_json
+  end
+
+  #customer_reviews routes
+  get "/customer_reviews" do
+    reviews = customer_reviews.all
+    reviews.to_json
+  end
+
+  post "/customer_reviews" do
+    reviews = customer_reviews.create(
+      comment: params[:comment]
+    )
+    reviews.to_json
+  end
+
+  patch "/customer_reviews/:id" do
+    reviews = customer_reviews.find(params[:id])
+    reviews.update(
+      comment: params[:comment]
+    )
+    reviews.to_json
+  end
+
+  delete "/customer_reviews/:id" do
+    reviews = customer_reviews.find(params[:id])
+    reviews.destroy
+    reviews.to_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@ puts "Creating hotels..."
 kempinski = Hotel.create(name: "Kempinski", location: "Nairobi, Kenya", price: "50000")
 olesereni = Hotel.create(name: "Olesereni", location: "Nairobi, Kenya", price: "40000")
 hilton = Hotel.create(name: "Hilton", location: "Nairobi, Kenya", price: "60000")
+serena = Hotel.create(name: "Serena", location: "Nairobi, Kenya", price: "70000")
+west = Hotel.create(name: "West Wood Hotel", location: "Nairobi, Kenya", price: "550000")
 
 puts "Creating customer_reviews"
 review1 = CustomerReview.create(comment: "Good place to stay. Clean place, modern room and nice staff", hotel_id: kempinski.id)
 review2 = CustomerReview.create(comment: "Homely stay with lovely stay. Breakfast was tasty with lots of variety. Great location next to all of Nairobi's main attractions", hotel_id: olesereni.id)
 review3 = CustomerReview.create(comment: "The whole compound is like paradise with vast and very beautiful and peaceful garden", hotel_id: hilton.id)
-# review4 = CustomerReview.create(comment: "The most beautiful guest house with the most lovely, warm and welcoming hosts")
-# review5 = CustomerReview.create(comment: "Really good breakfast, cozy rooms, good beds and very nice and helpful owners")
+review4 = CustomerReview.create(comment: "The most beautiful guest house with the most lovely, warm and welcoming hosts", hotel_id: serena.id)
+review5 = CustomerReview.create(comment: "Really good breakfast, cozy rooms, good beds and very nice and helpful owners", hotel_id: west.id)
 
 puts "✅ Done seeding!"
