ea5e03c1af142df077d4c8b03a098d009bf76afd
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-06 13:48:19 +0300

    make changes on routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,51 +2,51 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  #Hotel routes
+  # Hotel routes
   get "/hotels" do
-    hotels = hotel.all
+    hotels = Hotel.all
     hotels.to_json
   end
 
   post "/hotels" do
-    hotel = hotel.create(
+    reserved = Hotel.create(
       name: params[:name],
       location: params[:location],
+      price: params[:price]
+    )
+    reserved.to_json
+  end
+
+  patch "/hotels/:id" do
+    hotel = Hotel.find(params[:id])
+    hotel.update(
+      name: params[:name],
       price: params[:price]
     )
     hotel.to_json
   end
 
-  patch "/hotels/:id" do
-    hotels = hotels.find(params[:id])
-    hotels.update(
-      name: params[:name],
-      price: params[:price]
-    )
-    hotels.to_json
-  end
-
   delete "/hotels/:id" do
-    hotels = hotels.find(params[:id])
-    hotels.destroy
-    hotels.to_json
+    hotel = Hotel.find(params[:id])
+    hotel.destroy
+    hotel.to_json
   end
 
-  #customer_reviews routes
+  # customer_reviews routes
   get "/customer_reviews" do
-    reviews = customer_reviews.all
+    reviews = CustomerReview.all
     reviews.to_json
   end
 
   post "/customer_reviews" do
-    reviews = customer_reviews.create(
+    new_review = CustomerReview.create(
       comment: params[:comment]
     )
-    reviews.to_json
+    new_review.to_json
   end
 
   patch "/customer_reviews/:id" do
-    reviews = customer_reviews.find(params[:id])
+    reviews = CustomerReview.find(params[:id])
     reviews.update(
       comment: params[:comment]
     )
@@ -54,7 +54,7 @@ class ApplicationController < Sinatra::Base
   end
 
   delete "/customer_reviews/:id" do
-    reviews = customer_reviews.find(params[:id])
+    reviews = CustomerReview.find(params[:id])
     reviews.destroy
     reviews.to_json
   end
