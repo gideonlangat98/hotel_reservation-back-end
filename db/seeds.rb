c3153270b03729db5a08a10d9b91f8d514079ea7
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-06 12:38:24 +0300

    make connection to hotels and customer_reviews using foreign keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,10 @@ olesereni = Hotel.create(name: "Olesereni", location: "Nairobi, Kenya", price: "
 hilton = Hotel.create(name: "Hilton", location: "Nairobi, Kenya", price: "60000")
 
 puts "Creating customer_reviews"
-review1 = CustomerReview.create(comment: "Good place to stay ·  Clean place, modern room and nice staff")
-review2 = CustomerReview.create(comment: "Homely stay with lovely stay. Breakfast was tasty with lots of variety. Great location next to all of Nairobi's main attractions")
-review3 = CustomerReview.create(comment: "The whole compound is like paradise with vast and very beautiful and peaceful garden")
-review4 = CustomerReview.create(comment: "The most beautiful guest house with the most lovely, warm and welcoming hosts")
-review5 = CustomerReview.create(comment: "Really good breakfast, cozy rooms, good beds and very nice and helpful owners")
-
+review1 = CustomerReview.create(comment: "Good place to stay. Clean place, modern room and nice staff", hotel_id: kempinski.id)
+review2 = CustomerReview.create(comment: "Homely stay with lovely stay. Breakfast was tasty with lots of variety. Great location next to all of Nairobi's main attractions", hotel_id: olesereni.id)
+review3 = CustomerReview.create(comment: "The whole compound is like paradise with vast and very beautiful and peaceful garden", hotel_id: hilton.id)
+# review4 = CustomerReview.create(comment: "The most beautiful guest house with the most lovely, warm and welcoming hosts")
+# review5 = CustomerReview.create(comment: "Really good breakfast, cozy rooms, good beds and very nice and helpful owners")
 
 puts "✅ Done seeding!"
