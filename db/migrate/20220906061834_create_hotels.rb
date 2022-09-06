6d928752e93b12411550384431b5893deff74556
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-06 09:41:42 +0300

    create tables and models with inheritance from the active records

--- db/migrate/20220906061834_create_hotels.rb
+++ db/migrate/20220906061834_create_hotels.rb
@@ -1,4 +1,10 @@
 class CreateHotels < ActiveRecord::Migration[6.1]
   def change
+    create_tables :hotels do |t|
+      t.string :name
+      t.string :location
+      t.integer :price
+      t.timestamps
+    end
   end
 end
--- db/migrate/20220906061926_create_customer_reviews.rb
+++ db/migrate/20220906061926_create_customer_reviews.rb
@@ -1,4 +1,9 @@
 class CreateCustomerReviews < ActiveRecord::Migration[6.1]
   def change
+    create_tables :customer_reviews do |t|
+      t.string :comment
+      t.integer :hotel_id
+      t.timestamps
+    end
   end
 end
