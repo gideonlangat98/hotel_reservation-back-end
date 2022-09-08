1a3897daca42f31bddbbab5749013ee57a7c3466
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-08 13:42:11 +0300

    fixed post request error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,60 +3,60 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   # Hotel routes
-  # get "/hotels" do
-  #   hotels = Hotel.all
-  #   hotels.to_json
-  # end
+  get "/hotels" do
+    hotels = Hotel.all
+    hotels.to_json
+  end
 
-  # post "/hotels" do
-  #   reserved = Hotel.create(
-  #     name: params[:name],
-  #     location: params[:location],
-  #     price: params[:price]
-  #   )
-  #   reserved.to_json
-  # end
+  post "/hotels" do
+    reserved = Hotel.create(
+      name: params[:name],
+      location: params[:location],
+      price: params[:price]
+    )
+    reserved.to_json
+  end
 
-  # patch "/hotels/:id" do
-  #   hotel = Hotel.find(params[:id])
-  #   hotel.update(
-  #     name: params[:name],
-  #     price: params[:price]
-  #   )
-  #   hotel.to_json
-  # end
+  patch "/hotels/:id" do
+    hotel = Hotel.find(params[:id])
+    hotel.update(
+      name: params[:name],
+      price: params[:price]
+    )
+    hotel.to_json
+  end
 
-  # delete "/hotels/:id" do
-  #   hotel = Hotel.find(params[:id])
-  #   hotel.destroy
-  #   hotel.to_json
-  # end
+  delete "/hotels/:id" do
+    hotel = Hotel.find(params[:id])
+    hotel.destroy
+    hotel.to_json
+  end
 
-  # # customer_reviews routes
-  # get "/customer_reviews" do
-  #   reviews = CustomerReview.all
-  #   reviews.to_json
-  # end
+  # customer_reviews routes
+  get "/reviews" do
+    reviews = Review.all
+    reviews.to_json
+  end
 
-  # post "/customer_reviews" do
-  #   new_review = CustomerReview.create(
-  #     comment: params[:comment]
-  #   )
-  #   new_review.to_json
-  # end
+  post "/reviews" do
+    new_review = Review.create(
+      body: params[:body]
+    )
+    new_review.to_json
+  end
 
-  # patch "/customer_reviews/:id" do
-  #   reviews = CustomerReview.find(params[:id])
-  #   reviews.update(
-  #     comment: params[:comment]
-  #   )
-  #   reviews.to_json
-  # end
+  patch "/reviews/:id" do
+    reviews = Review.find(params[:id])
+    reviews.update(
+      comment: params[:comment]
+    )
+    reviews.to_json
+  end
 
-  # delete "/customer_reviews/:id" do
-  #   reviews = CustomerReview.find(params[:id])
-  #   reviews.destroy
-  #   reviews.to_json
-  # end
+  delete "/reviews/:id" do
+    reviews = Review.find(params[:id])
+    reviews.destroy
+    reviews.to_json
+  end
   
 end
