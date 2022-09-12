3e9d2d71121608d3becea1e4a510d4fd7246d2b8
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-12 15:26:25 +0300

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,16 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 puts "Creating hotels..."
-kempinski = Hotel.create(name: "Kempinski", location: "Nairobi, Kenya", price: "50000")
-olesereni = Hotel.create(name: "Olesereni", location: "Nairobi, Kenya", price: "40000")
-hilton = Hotel.create(name: "Hilton", location: "Nairobi, Kenya", price: "60000")
-serena = Hotel.create(name: "Serena", location: "Nairobi, Kenya", price: "70000")
-west = Hotel.create(name: "West Wood Hotel", location: "Nairobi, Kenya", price: "550000")
+room1 = Hotel.create(name: "Deluxe room", location: "Nairobi, Kenya", price: "30000")
+room2 = Hotel.create(name: "single room", location: "Nairobi, Kenya", price: "40000")
+room3 = Hotel.create(name: "Family room", location: "Nairobi, Kenya", price: "60000")
+room4 = Hotel.create(name: "Twin Bed room", location: "Nairobi, Kenya", price: "70000")
 
 puts "Creating customer_reviews"
-review1 = Review.create(body: "Good place to stay. Clean place, modern room and nice staff", hotel_id: hilton.id)
-review2 = Review.create(body: "Homely stay with lovely stay. Breakfast was tasty with lots of variety. Great location next to all of Nairobi's main attractions", hotel_id: olesereni.id)
-review3 = Review.create(body: "The whole compound is like paradise with vast and very beautiful and peaceful garden", hotel_id: kempinski.id)
-review4 = Review.create(body: "The most beautiful guest house with the most lovely, warm and welcoming hosts", hotel_id: west.id)
-review5 = Review.create(body: "Really good breakfast, cozy rooms, good beds and very nice and helpful owners", hotel_id: serena.id)
+review1 = Review.create(body: "Good place to stay. Clean place, modern room and nice staff", hotel_id: room1.id)
+review2 = Review.create(body: "Homely stay with lovely stay. Breakfast was tasty with lots of variety. Great location next to all of Nairobi's main attractions", hotel_id: room2.id)
+review3 = Review.create(body: "The whole compound is like paradise with vast and very beautiful and peaceful garden", hotel_id: room3.id)
+review4 = Review.create(body: "The most beautiful guest house with the most lovely, warm and welcoming hosts", hotel_id: room3.id)
+review5 = Review.create(body: "Really good breakfast, cozy rooms, good beds and very nice and helpful owners", hotel_id: room4.id)
 
 puts "✅ Done seeding!"
